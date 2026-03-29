a9f0849ff9b78d86f9a708cf0cf4dc93cfe2903b
Michaela <michaela@leadingwithnice.com>
2026-03-29 01:59:27 +0000

    fix: correct bottom nav JSX placement (was outside wrapper div)

--- app/page.tsx
+++ app/page.tsx
@@ -516,7 +516,6 @@ export default function LandingPage() {
           </p>
         </div>
       </footer>
-    </div>
 
       {/* Bottom Navigation Preview - Links to sign in */}
       <nav className="fixed bottom-0 left-0 right-0 bg-white dark:bg-[#1a1a2e] border-t border-gray-200 dark:border-gray-800 py-2 px-4 z-50">
@@ -553,6 +552,7 @@ export default function LandingPage() {
 
       {/* Spacer for fixed bottom nav */}
       <div className="h-20"></div>
+    </div>
 
   )
 }
